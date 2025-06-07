9c574edc61530b86dde30b299e99aaadb590fe1d
Noel Alonso <todosenuso4@gmail.com>
2025-06-06 19:53:30 -0600

    Subida inicial del proyecto

---
+++ Data/Modelos/Producto.cs
@@ -0,0 +1,13 @@
+﻿namespace Proyecto_Taqueria.Data.Modelos
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Categoria { get; set; } // Ej: "Alimentos", "Limpieza"
+        public int CantidadStock { get; set; }
+        public int MinimoStock { get; set; } // Alerta cuando el stock sea menor a este número
+        public int MaximoStock { get; set; }
+        public DateTime FechaCaducidad { get; set; } // Para alertas
+    }
+}
